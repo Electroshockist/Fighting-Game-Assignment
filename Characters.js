2b50c6d92da65b630f2564f149c355302de394a5
kidhadez <32069896+kidhadez@users.noreply.github.com>
2018-04-26 18:16:03 -0400

    Add files via upload

--- Characters.js
+++ Characters.js
@@ -89,25 +89,84 @@ function Entity(IMAGE) {
 //character stuff
 var Cammy =  new Entity("Assets/Sprite Sheets/cammy.png");
 Cammy.facingRight.idle    = new animation(Cammy, 5, 0);
-Cammy.facingRight.move    = new animation(Cammy, 5, 1);
+Cammy.facingRight.move    = new animation(Cammy, 6, 1);
 Cammy.facingRight.jump    = new animation(Cammy, 5, 2);
-Cammy.facingRight.crouch  = new animation(Cammy, 5, 3);
+Cammy.facingRight.crouch  = new animation(Cammy, 1, 3);
 Cammy.facingRight.punch   = new animation(Cammy, 5, 4);
 Cammy.facingRight.kick    = new animation(Cammy, 5, 5);
-Cammy.facingRight.gethit  = new animation(Cammy, 5, 6);
+Cammy.facingRight.gethit  = new animation(Cammy, 4, 6);
 Cammy.facingRight.die     = new animation(Cammy, 5, 7);
 Cammy.facingRight.win     = new animation(Cammy, 5, 8);
 
 Cammy.facingLeft.idle    = new animation(Cammy, 5, 0);
-Cammy.facingLeft.move    = new animation(Cammy, 5, 1);
+Cammy.facingLeft.move    = new animation(Cammy, 6, 1);
 Cammy.facingLeft.jump    = new animation(Cammy, 5, 2);
-Cammy.facingLeft.crouch  = new animation(Cammy, 5, 3);
+Cammy.facingLeft.crouch  = new animation(Cammy, 1, 3);
 Cammy.facingLeft.punch   = new animation(Cammy, 5, 4);
 Cammy.facingLeft.kick    = new animation(Cammy, 5, 5);
-Cammy.facingLeft.gethit  = new animation(Cammy, 5, 6);
+Cammy.facingLeft.gethit  = new animation(Cammy, 4, 6);
 Cammy.facingLeft.die     = new animation(Cammy, 5, 7);
 Cammy.facingLeft.win     = new animation(Cammy, 5, 8);
 
 var ChunLi = new Entity("Assets/Sprite Sheets/chunli.png");
+ChunLi.facingRight.idle = new animation(ChunLi, 4, 0);
+ChunLi.facingRight.move = new animation(ChunLi, 8, 1);
+ChunLi.facingRight.jump = new animation(ChunLi, 4, 2);
+ChunLi.facingRight.crouch = new animation(ChunLi, 1, 3);
+ChunLi.facingRight.punch = new animation(ChunLi, 3, 4);
+ChunLi.facingRight.kick = new animation(ChunLi, 7, 5);
+ChunLi.facingRight.gethit = new animation(ChunLi, 2, 6);
+ChunLi.facingRight.die = new animation(ChunLi, 4, 7);
+ChunLi.facingRight.win = new animation(ChunLi, 3, 8);
+
+ChunLi.facingLeft.idle = new animation(ChunLi, 4, 0);
+ChunLi.facingLeft.move = new animation(ChunLi, 8, 1);
+ChunLi.facingLeft.jump = new animation(ChunLi, 4, 2);
+ChunLi.facingLeft.crouch = new animation(ChunLi, 1, 3);
+ChunLi.facingLeft.punch = new animation(ChunLi, 3, 4);
+ChunLi.facingLeft.kick = new animation(ChunLi, 7, 5);
+ChunLi.facingLeft.gethit = new animation(ChunLi, 2, 6);
+ChunLi.facingLeft.die = new animation(ChunLi, 4, 7);
+ChunLi.facingLeft.win = new animation(ChunLi, 3, 8);
+
 var Ken = new Entity("Assets/Sprite Sheets/ken.png");
+Ken.facingRight.idle = new animation(Ken, 4, 0);
+Ken.facingRight.move = new animation(Ken, 5, 1);
+Ken.facingRight.jump = new animation(Ken, 5, 2);
+Ken.facingRight.crouch = new animation(Ken, 1, 3);
+Ken.facingRight.punch = new animation(Ken, 5, 4);
+Ken.facingRight.kick = new animation(Ken, 5, 5);
+Ken.facingRight.gethit = new animation(Ken, 4, 6);
+Ken.facingRight.die = new animation(Ken, 5, 7);
+Ken.facingRight.win = new animation(Ken, 3, 8);
+
+Ken.facingLeft.idle = new animation(Ken, 4, 0);
+Ken.facingLeft.move = new animation(Ken, 5, 1);
+Ken.facingLeft.jump = new animation(Ken, 5, 2);
+Ken.facingLeft.crouch = new animation(Ken, 1, 3);
+Ken.facingLeft.punch = new animation(Ken, 5, 4);
+Ken.facingLeft.kick = new animation(Ken, 5, 5);
+Ken.facingLeft.gethit = new animation(Ken, 4, 6);
+Ken.facingLeft.die = new animation(Ken, 5, 7);
+Ken.facingLeft.win = new animation(Ken, 3, 8);
+
 var Ryu = new Entity("Assets/Sprite Sheets/ryu.png");
+Ryu.facingRight.idle = new animation(Ryu, 4, 0);
+Ryu.facingRight.move = new animation(Ryu, 5, 1);
+Ryu.facingRight.jump = new animation(Ryu, 5, 2);
+Ryu.facingRight.crouch = new animation(Ryu, 1, 3);
+Ryu.facingRight.punch = new animation(Ryu, 5, 4);
+Ryu.facingRight.kick = new animation(Ryu, 5, 5);
+Ryu.facingRight.gethit = new animation(Ryu, 4, 6);
+Ryu.facingRight.die = new animation(Ryu, 5, 7);
+Ryu.facingRight.win = new animation(Ryu, 3, 8);
+
+Ryu.facingLeft.idle = new animation(Ryu, 4, 0);
+Ryu.facingLeft.move = new animation(Ryu, 5, 1);
+Ryu.facingLeft.jump = new animation(Ryu, 5, 2);
+Ryu.facingLeft.crouch = new animation(Ryu, 1, 3);
+Ryu.facingLeft.punch = new animation(Ryu, 5, 4);
+Ryu.facingLeft.kick = new animation(Ryu, 5, 5);
+Ryu.facingLeft.gethit = new animation(Ryu, 4, 6);
+Ryu.facingLeft.die = new animation(Ryu, 5, 7);
+Ryu.facingLeft.win = new animation(Ryu, 3, 8);
